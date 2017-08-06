8b8044bfba7a5e95125d1d45decee26a7480e08e
Cam <cam.schnackel@gmail.com>
2017-08-06 16:38:07 -0500

    Finishes doWhileLoops.js and loopsAndConditions.js for testing

--- assignment/7_loopsAndConditions.js
+++ assignment/7_loopsAndConditions.js
@@ -5,8 +5,15 @@ function onlyEvens(numberArray) {
   // complete this function so that given an array full of
   // numbers (numberArray), it returns a new array containing
   // only the even ones. for example `onlyEvens([1,2,3,4]) === [2,4]`
+  for (var i = 0; i < numberArray.length; i++) {
+    if (numberArray[i] % 2 === 0) {
+      evensArray.push(numberArray[i]);
+    }
+  }
 
   return evensArray;
 }
 
 // test your function onlyEvens by calling it below and logging the result
+var testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(onlyEvens(testArray));
