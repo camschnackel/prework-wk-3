3dd6e7f9a12717754f58180f0e111ae3647cd5b0
Cam <cam.schnackel@gmail.com>
2017-08-06 15:31:47 -0500

    Initial draft of required assignments for testing

--- assignment/1_arrayIndexConsiderations.js
+++ assignment/1_arrayIndexConsiderations.js
@@ -5,17 +5,22 @@ var colors = ['red', 'blue', 'yellow'];
 // using array indices, write an expression that will evaluate to the color described by these variable names
 // for example var theColorBlue = colors[1];
 // uncomment the next two lines and replace ____ with the appropriate code
-// var theColorRed = _____;
-// var theColorYellow = _____;
+var theColorRed = colors[0];
+var theColorYellow = colors[2];
 
-// what happens if we access an index that has no element?
+// what happens if we access an index that has no element? It will say undefined
 // uncomment the following line and replace ____ with the appropriate code
-// var fortyThirdColor = _____;
+var fortyThirdColor = colors[42];
 
 function lastItem( lastItemArray ) {
   // this function accepts an array (lastItemArray) as an argument
   // have it return the last item in the array
+  var lastItem = lastItemArray[lastItemArray.length - 1]
+  return lastItem
+
 }
 
 // test your function lastItem by calling it below and logging the result
-// for ex. console.log(lastItem(colors)); 
+// for ex. console.log(lastItem(colors));
+console.log(lastItem(colors));
+console.log(fortyThirdColor);
--- assignment/4_forLoop.js
+++ assignment/4_forLoop.js
@@ -2,15 +2,20 @@ function shoesOnTheBus( ArrayOfKidsOnBus ){
   // This function accepts an array (ArrayOfKidsOnBus) of kids on the bus.
   // Assume each kid on the bus is wearing 2 shoes.
   var shoes = 0; // start with zero shoes
-
+  for (var i = 0; i < ArrayOfKidsOnBus.length; i++) {
+    console.log("Nice Shoes " + bus[i] +"!");
+    var shoes = (i + 1) * 2;
+  }
   // Write a for loop to count how many shoes are on the bus
   // AND console log "Nice shoes!" for each student.
 
   // The totaling of shoes should be done inside the loop.
-
+return shoes
 
   // This function should return the number of shoes on the bus.
 
 } // end shoesOnTheBus
 
 // test your function shoesOnTheBus by calling it below and logging the result
+var bus = ["Mike", "Beth", "Carl", "Amanda", "Sam"];
+console.log(shoesOnTheBus(bus));
